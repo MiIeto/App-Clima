1058cbcad99522bfb5e12d0c373d0393687fcd09
Mileto <milenkotomasevic41@gmail.com>
2024-08-03 02:19:28 -0400

    dev: Added spinner and initial state

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -37,19 +37,24 @@ export type Weather = z.infer<typeof Weather>;
 // 	}),
 // });
 
-export default function useWeather() {
+const initialState = {
+	name: '',
+	main: {
+		temp: 0,
+		temp_max: 0,
+		temp_min: 0,
+	}
+}
 
-	const [weather, setWeather] = useState<Weather>({
-		name: '',
-		main: {
-			temp: 0,
-			temp_max: 0,
-			temp_min: 0,
-		},
-	});
+export default function useWeather() {
+	const [weather, setWeather] = useState<Weather>(initialState);
+
+	const [loading, setLoading] = useState(false);
 
 	const fetchWeather = async (search: SearchType) => {
 		const appId = import.meta.env.VITE_API_KEY;
+		setLoading(true);
+		setWeather(initialState);
 		try {
 			const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
 			const { data } = await axios.get(geoUrl);
@@ -76,7 +81,7 @@ export default function useWeather() {
 			const result = Weather.safeParse(weatherResult);
 			if (result.success) {
 				setWeather(result.data);
-			} 
+			}
 
 			// Valibot
 			// const { data: weatherResult } = await axios(weatherUrl);
@@ -86,14 +91,17 @@ export default function useWeather() {
 			// }
 		} catch (error) {
 			console.log(error);
+		} finally {
+			setLoading(false);
 		}
 	};
 
-	const hasWeatherData = useMemo(() => weather.name, [weather])
+	const hasWeatherData = useMemo(() => weather.name, [weather]);
 
 	return {
 		weather,
+		loading,
 		fetchWeather,
-		hasWeatherData
+		hasWeatherData,
 	};
 }
