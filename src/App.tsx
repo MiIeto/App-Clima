760a44e6ca3ab588ecc19c10f84ce16c5050f089
Mileto <milenkotomasevic41@gmail.com>
2024-08-03 01:59:30 -0400

    dev: Added verification

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import WeatherDetail from './components/WeatherDetail/WeatherDetail';
 import useWeather from './hooks/useWeather';
 
 function App() {
-	const { weather, fetchWeather } = useWeather();
+	const { weather, fetchWeather, hasWeatherData } = useWeather();
 	return (
 		<>
 			<h1 className={styles.title}>Buscador de clima</h1>
 			<div className={styles.container}>
 				<Form fetchWeather={fetchWeather} />
-				<WeatherDetail weather={weather} />
+				{hasWeatherData && <WeatherDetail weather={weather} />}
 			</div>
 		</>
 	);
