86970fb65650672addbcbe632496c553d99c6759
Mileto <milenkotomasevic41@gmail.com>
2024-08-03 02:26:28 -0400

    dev: Added comprobation of city in country

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -43,21 +43,30 @@ const initialState = {
 		temp: 0,
 		temp_max: 0,
 		temp_min: 0,
-	}
-}
+	},
+};
 
 export default function useWeather() {
 	const [weather, setWeather] = useState<Weather>(initialState);
 
 	const [loading, setLoading] = useState(false);
 
+	const [notFound, setNotFound] = useState(false);
+
 	const fetchWeather = async (search: SearchType) => {
 		const appId = import.meta.env.VITE_API_KEY;
 		setLoading(true);
 		setWeather(initialState);
+		setNotFound(false);
 		try {
 			const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
 			const { data } = await axios.get(geoUrl);
+
+			// Comprobar si existe la ciudad
+			if (!data[0]) {
+				setNotFound(true);
+				return;
+			}
 
 			const lat = data[0].lat;
 			const lon = data[0].lon;
@@ -101,6 +110,7 @@ export default function useWeather() {
 	return {
 		weather,
 		loading,
+		notFound,
 		fetchWeather,
 		hasWeatherData,
 	};
