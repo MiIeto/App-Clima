f27fc20422872d7ff60e17c81d29719ca66ed473
Mileto <milenkotomasevic41@gmail.com>
2024-08-06 01:08:07 -0400

    dev: Added skeleton to loader

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,5 +1,9 @@
 import styles from './Spinner.module.css';
 
 export default function Spinner() {
-	return <div className={styles.loader}></div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.loader}></div>
+		</div>
+	);
 }
